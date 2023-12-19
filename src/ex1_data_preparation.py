d909075a7c988944f471fd1120f22cc5ed3d3a2d
Jonathan <jonathan.woerner@gmx.de>
2023-12-19 18:53:55 +0100

    .

--- src/ex1_data_preparation.py
+++ src/ex1_data_preparation.py
@@ -1,7 +1,14 @@
-def filter_block(block) -> bool:
+from typing import List
+import os
+
+def filter_block(block: List[str]) -> bool:
+    # check if the block has at least 4 lines
+    #and check if the first line starts with #*, the second with #@, the third with #t, the fourth with #c and the fifth with #index
+    if len(block) < 5 or not block[0].startswith("#*") or not block[1].startswith("#@") or not block[2].startswith("#t") or not block[3].startswith("#c") or not block[4].startswith("#index"):
+        return False
     #first filter by date
-    #take the third line and get characters 3-6 as a number
-    year = int(block[2][3:7])
+    #take the third line and get characters 2-5 as a number
+    year = int(block[2][2:6])
     # if the year is not between 1995 and 2004 return
     if year < 1995 or year > 2004:
         return False
@@ -9,18 +16,22 @@ def filter_block(block) -> bool:
     venue = block[3][1:]
     if "SIGMOD" not in venue.upper() and "VLDB" not in venue.upper():
         return False
+    return True
     
-def map_block(block) -> dict:
+def map_block(block: List[str]) -> dict:
     # get the title
     title = block[0][2:]
     # get the authors
-    authors = block[1][2:].split(",").strip().join(", ")
+    authors = block[1][2:].split(",")
+    for i in range(len(authors)):
+        authors[i] = authors[i].strip()
+    authors = ", ".join(authors)
     # get the year
-    year = int(block[2][3:7])
+    year = int(block[2][2:6])
     # get the publication venue
     venue = block[3][1:]
     # get the index
-    index = int(block[4][6:])
+    index = block[4][6:]
     # create a dictionary with the mapped values
     mapped_block = {
         "title": title,
@@ -31,15 +42,17 @@ def map_block(block) -> dict:
     }
     return mapped_block  
 
-def save_block(block, file_name):
+def save_block(block, file_name: str):
     # open the file in append mode
-    with open(file_name, 'a') as file:
+    with open(file_name, 'a', encoding='utf-8') as file:
         # create a string in csv format
         string = f"{block['title']};{block['authors']};{block['year']};{block['venue']};{block['index']}\n"
+        # write the string to the file
+        file.write(string)
 
 
 
-def process_block(block):
+def process_block(block: List[str]):
     # filter unwanted blocks
     if not filter_block(block):
         return None
@@ -48,9 +61,15 @@ def process_block(block):
 
 
 def process_file(inputFileName: str, outputFileName: str):
+# delete the output file if it exists
+    try:
+        os.remove(outputFileName)
+    except OSError:
+        pass
 # Open in read mode
-    with open(inputFileName, 'r') as file:
-        block = []
+    with open(inputFileName, 'r', encoding='utf-8') as file:
+        #declare block as an empty string array
+        block: List[str] = []
 
         for line in file:
             # If the line is empty, it means the end of a block
@@ -75,7 +94,7 @@ def process_file(inputFileName: str, outputFileName: str):
 
 def main():
     process_file('data/dblp.txt', 'data/DBLP_1995_2004.csv')
-    process_file('data/citation-acm-v8.txt', 'data/ACM_1995_2004.csv')
+   # process_file('data/citation-acm-v8.txt', 'data/ACM_1995_2004.csv')
 
 if __name__ == '__main__':
     main()
